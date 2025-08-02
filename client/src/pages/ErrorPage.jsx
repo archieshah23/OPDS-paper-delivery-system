a6dbb513c66b60c95affbf6537dff87b5f803b91
archie911 <archieshah2303@gmail.com>
2025-08-02 21:43:46 +0530

    errorpage

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,11 +1,18 @@
 import errorimg from "../assets/errorpage.jpg";
+import "../design/error.css";
+
 export const ErrorPage = () => {
   return (
-    <div>
-      <h1>Oops!! </h1>
-      <h1>something went wrong!!</h1>
-      <h2>404 page not found</h2>
-      <img src={errorimg} alt="error" />
+    <div className="error-container">
+      <h1 className="error-title">Oops!</h1>
+      <p className="error-subtitle">Something went wrong...</p>
+      <h2 className="error-code">404 - Page Not Found</h2>
+
+      <img src={errorimg} alt="Error" className="error-image" />
+
+      <a href="/" className="error-btn">
+        Go Back Home
+      </a>
     </div>
   );
 };
